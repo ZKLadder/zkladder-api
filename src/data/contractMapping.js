13e784a5b8a69b57d5ad4e186190250a65fb7d18
Justin Moroz <justin.moroz@finteractions.com>
2021-11-23 08:25:07 -0500

    feat: add support for erc20-mintable-ownable (#4)
    * feat: add support for erc20-mintable-ownable
    
    * fix: update erc721 contract name

--- src/data/contractMapping.js
+++ src/data/contractMapping.js
@@ -1,7 +1,12 @@
 module.exports = {
   1: {
-    name: 'ERC721Enumerable',
+    name: 'ERC721_Enumerable',
     id: 1,
-    src: 'src/contracts/ERC721Enumerable.sol',
+    src: 'src/contracts/ERC721_Enumerable.sol',
+  },
+  2: {
+    name: 'ERC20_Ownable_Mintable',
+    id: 2,
+    src: 'src/contracts/ERC20_Ownable_Mintable.sol',
   },
 };
