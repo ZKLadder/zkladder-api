c78b1e8f090593d1ba6625e2eddc8a28a7935cfb
Justin Moroz <justin.moroz@finteractions.com>
2022-06-02 11:22:42 -0400

    feat: remove all deployment logic from the api (#45)

--- src/routes/v1/contracts.js
+++ src/routes/v1/contracts.js
@@ -1,8 +1,4 @@
 const express = require('express');
-const { ClientError } = require('../../utils/error');
-const { deployContract } = require('../../services/deploy');
-const { getContractById } = require('../../utils/contract');
-const { generateContractABI } = require('../../services/compile');
 const { createContract, getContracts } = require('../../services/contract');
 const authentication = require('../middleware/authentication');
 
@@ -21,37 +17,6 @@ router.get('/', authentication, async (req, res, next) => {
   try {
     const contracts = await getContracts(req.query);
     res.send(contracts);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.post('/deploy', async (req, res, next) => {
-  try {
-    const { contractId, constructParams, networkId } = req.body;
-    if (!contractId || !constructParams || !networkId) {
-      throw new ClientError('Missing required body field');
-    }
-    const contractData = await deployContract(contractId, networkId, constructParams);
-    res.send(contractData);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get('/:contractId/abi', async (req, res, next) => {
-  try {
-    const { contractId } = req.params;
-    if (!contractId) {
-      throw new ClientError('Missing required body field');
-    }
-    const { name } = getContractById(contractId);
-    const compiledContract = generateContractABI(contractId);
-    const { abi } = compiledContract;
-    const bytecode = compiledContract.evm.bytecode.object;
-    res.send({
-      name, contractId, abi, bytecode,
-    });
   } catch (error) {
     next(error);
   }
